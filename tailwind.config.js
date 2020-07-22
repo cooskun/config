1419088543dc5f121a243f0060a3e21d597aac03
coskunbaris <coskun.bris@gmail.com>
2020-07-22 12:00:16 +0200

    Init Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js'],
+  theme: {
+    extend: {
+      opacity: {
+        '60': '.6',
+      },
+      width: {
+        '9/10': '90%',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
